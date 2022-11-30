d0c6242a83c6c482fc8646b2ab5cc43e2f39ccbe
tychoSRC <99697508+tychoSRC@users.noreply.github.com>
2022-11-30 12:34:50 +0100

    Update U3_A2.c

--- U3/U3_A2.c
+++ U3/U3_A2.c
@@ -15,11 +15,12 @@
 int main()                                    // M  ain function
   {
  
-   fdserial *toTerm = fdserial_open(TRx,TTx, MODE ,BAUDRATE);
-   fdserial *loopBack = fdserial_open(Rx, Tx, MODE, BAUDRATE);
+   fdserial *TermCon = fdserial_open(TRx,TTx, MODE ,BAUDRATE);
+   fdserial *wifiCon = fdserial_open(Rx, Tx, MODE, BAUDRATE);
 
    char c;
-   char c2;
+   char c2 = 'A';
+   char c3 = 'A';
    char arrOut[50];
    char arrIn[50];
    int count = 1; 
@@ -34,14 +35,53 @@ int main()                                    // M  ain function
    
 
    while(1){
-    
+    c2 = fdserial_rxCheck(TermCon);
+    if(c2 != -1){ 
     //if(c!=-1){
-      c = fdserial_rxChar(toTerm);
-      arrIn[0] = c; 
+      c = fdserial_rxChar(TermCon);
+      arrIn[0] = c;
+      //c = fdserial_rxChar(TermCon);
+      //arrIn[0] = c; 
+
     //}      
       
       while(c != '\r'){
-        c = fdserial_rxChar(toTerm);
+        c = fdserial_rxChar(TermCon);
+        if(c != -1){
+          //fdserial_txChar(loopBack, c);
+          //arrOut[0] = 'z';
+          //arrIn[count] = fdserial_rxChar(loopBack);
+          arrIn[count] = c;
+          count++;
+          }
+        }
+        //fdserial_txChar(toTerm, '\r');
+        //fdserial_txChar(toTerm, arrOut[0]);
+        //fdserial_txChar(toTerm, '\r');
+        //c = ' ';
+        fdserial_rxFlush(TermCon);
+        //dprint(TermCon, "You typed: %c \r", arrIn[0]);
+        count = 1;
+     
+
+      
+     // if(c != -1){
+      length = strlen(arrIn);
+      for(int i = 0; i < length; i++){
+        fdserial_txChar(wifiCon, arrIn[i]);
+        arrIn[i]= 0;
+        }        
+     //}       
+   }  
+    c3 =fdserial_rxCheck(wifiCon);
+     if(c3 != -1){
+    //if(c!=-1){
+      c = fdserial_rxChar(wifiCon);
+      arrIn[0] = c; 
+    //}      
+      
+      while(c != '\n'){
+        c = fdserial_rxChar(wifiCon);
         if(c != -1){
           //fdserial_txChar(loopBack, c);
           //arrOut[0] = 'z';
@@ -54,18 +94,19 @@ int main()                                    // M  ain function
         //fdserial_txChar(toTerm, arrOut[0]);
         //fdserial_txChar(toTerm, '\r');
         //c = ' '; 
+                fdserial_rxFlush(wifiCon);
         count = 1;
      
 
       
-      if(c != -1){
+     // if(c != -1){
       length = strlen(arrIn);
       for(int i = 0; i < length; i++){
-        fdserial_txChar(loopBack, arrIn[i]);
+        fdserial_txChar(TermCon, arrIn[i]);
         arrIn[i]= 0;
         }        
-     }       
-         
+     //}       
+   }          
       
   }      
    return 0; 
