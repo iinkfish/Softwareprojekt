6a058b9c54e291c85027636d42b3cb6df862b730
tychoSRC <99697508+tychoSRC@users.noreply.github.com>
2022-12-06 14:30:36 +0100

    updated server

--- U3/javaServ/client.java
+++ U3/javaServ/client.java
@@ -2,43 +2,59 @@ package U3.javaServ;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws IOException {
         // String ip_address = "192.168.4.1";
         String ip_address = "localhost";
+        // while (true) {
+        Socket socket = new Socket(ip_address, 23);
 
-        Socket socket = new Socket(ip_address, 25);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        PrintWriter pr = new PrintWriter(socket.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); // reading from wifi
+                                                                                                    // module
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // writing to wifi
+                                                                                                      // module
+        // PrintWriter pr = new PrintWriter(socket.getOutputStream());
+        InputStreamReader r = new InputStreamReader(System.in); // reading from terminal
+        BufferedReader br = new BufferedReader(r);
+
+        // Scanner myInput = new Scanner(System.in);
+        String output = "";
 
         String line = "";
-        boolean isFound = true;
-        // Receiver:
-        while (isFound) {
-            try {
-                line = reader.readLine();
-                System.out.println("server: " + line);
-                // System.out.println(line);
-                isFound = line.indexOf("exit") != -1 ? false : true;
-            } catch (IOException e) {
-                e.printStackTrace();
+        while (true) {
+            boolean isFound = true;
+            // Receiver:
+            if (reader.ready()) {
+                try {
+                    line = reader.readLine(); // reads line from wifi module
+                    System.out.println("server: " + line);
+                    // System.out.println(line);
+                    // isFound = line.indexOf("exit") != -1 ? false : true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
 
-        // System.out.println("exit of while loop");
+            // System.out.println("exit of while loop");
 
-        // Sender
-        try {
-            // pr.println("String received");
-            // pr.flush();
-            String output = "String received";
-            writer.write(output);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Sender
+            if (br.ready()) {
+                try {
+                    // pr.println("String received");
+                    // pr.flush();
+                    // System.out.println("Your Input pls");
+                    output = br.readLine(); // reads line from this terminal
+                    // output = myInput.nextLine();
+                    writer.write(output); // sends line to wifi module
+                    writer.newLine();
+                    writer.flush();
+                    // writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- U3/javaServ/server.java
+++ U3/javaServ/server.java
@@ -2,28 +2,46 @@ package U3.javaServ;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class server {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket socket = new ServerSocket(25);
+        ServerSocket socket = new ServerSocket(23);
+        // while (true) {
         Socket s = socket.accept();
 
         System.out.println("client connected");
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+
+        Scanner myInput = new Scanner(System.in);
+        String output = "";
 
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("message1");
-        pr.flush();
-        pr.println("message2");
-        pr.flush();
-        pr.println("message3");
-        pr.flush();
-        pr.println("exit");
-        pr.flush();
 
-        String str = bf.readLine();
-        System.out.println("client: " + str);
+        writer.write("Are you there?");
+        writer.newLine();
+        writer.flush();
+        while (true) {
+
+            if (bf.ready()) {
+                String str = bf.readLine();
+                System.out.println("client: " + str);
+            }
+
+            if (br.ready()) {
+
+                output = br.readLine();
+                writer.write(output);
+                writer.newLine();
+                writer.flush();
+            }
+
+        }
     }
 }
